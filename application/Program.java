0d562149a1446eed066fbaa5db522a06918f7895
enzohbram <152501407+enzohbram@users.noreply.github.com>
2023-12-21 11:22:59 -0300

    Exception Handling

---
+++ application/Program.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+import model.exceptions.DomainException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		  Scanner sc = new Scanner(System.in);
+		  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+		  try {
+			  System.out.print("Room number: ");
+		      int number = sc.nextInt();
+		      System.out.print("Check-in date (dd/MM/yyyy): ");
+		      LocalDate checkIn = LocalDate.parse(sc.next(), formatter);
+		      System.out.print("Check-out date (dd/MM/yyyy): ");
+		      LocalDate checkOut = LocalDate.parse(sc.next(), formatter);
+
+		      Reservation reservation = new Reservation(number, checkIn, checkOut);
+		      System.out.println("Reservation: " + reservation);
+
+		      System.out.println();
+		      System.out.println("Enter data to update the reservation:");
+		      System.out.print("Check-in date (dd/MM/yyyy): ");
+		      checkIn = LocalDate.parse(sc.next(), formatter);
+		      System.out.print("Check-out date (dd/MM/yyyy): ");
+		      checkOut = LocalDate.parse(sc.next(), formatter);
+
+		      reservation.updateDates(checkIn, checkOut);
+		      System.out.println("Reservation: " + reservation);
+		      } catch (java.time.format.DateTimeParseException e) {
+		    	  System.out.println("Invalid date format");
+		      } catch (DomainException e) {
+		    	  System.out.println("Error in reservation: " + e.getMessage());
+		      } catch (RuntimeException e) {
+		    	  System.out.println("Unexpected error");
+		      } finally {
+		      
+		    	  sc.close();
+		      }
+		  
+	}
+		
+}
+
